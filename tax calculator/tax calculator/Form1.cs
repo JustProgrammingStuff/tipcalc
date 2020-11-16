f22ce3450c0c90ff5dc16ecd114782409aafd44c
Tom-Program <47488788+Tom-Program@users.noreply.github.com>
2020-11-15 20:49:57 -0500

    Add files via upload

---
+++ tax calculator/tax calculator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tax_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string split = textBox1.Text;
+            decimal tax = decimal.Parse(split) * decimal.Parse(textBox2.Text) / 100;
+            textBox3.Text = tax.ToString();
+        }
+    }
+}
